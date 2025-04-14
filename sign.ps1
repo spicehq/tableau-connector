9b9ecdc867aa112b8e56edb34a221f8ac2bb93c9
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-04-14 16:21:12 -0700

    Add powershell script

---
+++ sign.ps1
@@ -0,0 +1,11 @@
+echo $env:DIGICERT_TOKEN_PASSWORD | jarsigner -verbose `
+    -tsa http://timestamp.digicert.com `
+    -keystore NONE `
+    -storetype PKCS11 `
+    -storepass $env:DIGICERT_TOKEN_PASSWORD `
+    -providerClass sun.security.pkcs11.SunPKCS11 `
+    -providerArg "$env:DIGICERT_TOKEN_CFG_PATH" `
+    -sigalg SHA256withRSA `
+    -signedjar $env:SIGNED_BINARY_NAME `
+    spice.taco `
+    "$env:DIGICERT_KEY_ALIAS" 
