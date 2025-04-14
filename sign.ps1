037dafd8d5bd0d906189b68aea7014ea5fab4ab0
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-04-14 16:31:18 -0700

    Add error checks

--- sign.ps1
+++ sign.ps1
@@ -1,4 +1,23 @@
-echo $env:DIGICERT_TOKEN_CFG_PATH
+echo "Signing with Digicert Token"
+
+# Check if the required environment variables are set
+if (-not $env:DIGICERT_TOKEN_PASSWORD) {
+    Write-Host "Error: DIGICERT_TOKEN_PASSWORD environment variable is not set."
+    exit 1
+}
+if (-not $env:DIGICERT_TOKEN_CFG_PATH) {
+    Write-Host "Error: DIGICERT_TOKEN_CFG_PATH environment variable is not set."
+    exit 1
+}
+if (-not $env:DIGICERT_KEY_ALIAS) {
+    Write-Host "Error: DIGICERT_KEY_ALIAS environment variable is not set."
+    exit 1
+}
+if (-not $env:SIGNED_BINARY_NAME) {
+    Write-Host "Error: SIGNED_BINARY_NAME environment variable is not set."
+    exit 1
+}
+
 echo $env:DIGICERT_TOKEN_PASSWORD | jarsigner -verbose `
     -tsa http://timestamp.digicert.com `
     -keystore NONE `
